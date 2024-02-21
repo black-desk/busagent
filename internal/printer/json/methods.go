d0a7cce58b15867346627cd4d3bfac8e3f25c432
black-desk <me@black-desk.cn>
2024-02-21 16:42:22 +0800

    fix: use go-json-experiment to print dbus.Variant
    Signed-off-by: black-desk <me@black-desk.cn>

--- internal/printer/json/methods.go
+++ internal/printer/json/methods.go
@@ -1,8 +1,10 @@
 package json
 
 import (
-	"encoding/json"
 	"fmt"
+
+	json "github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
 
 	"github.com/godbus/dbus/v5"
 )
@@ -16,12 +18,8 @@ func (p *impl) Variant(variant *dbus.Variant) {
 		Value:     variant.Value(),
 	}
 	var err error
-	var raw json.RawMessage
-	if p.indent == "" {
-		raw, err = json.Marshal(v)
-	} else {
-		raw, err = json.MarshalIndent(v, "", p.indent)
-	}
+	var raw []byte
+	raw, err = json.Marshal(v, jsontext.WithIndent(p.indent))
 	if err != nil {
 		panic(err)
 	}
@@ -31,12 +29,19 @@ func (p *impl) Variant(variant *dbus.Variant) {
 
 func (p *impl) Signal(s *dbus.Signal) {
 	var err error
-	var raw json.RawMessage
-	if p.indent == "" {
-		raw, err = json.Marshal(s)
-	} else {
-		raw, err = json.MarshalIndent(s, "", p.indent)
-	}
+	var raw []byte
+	raw, err = json.Marshal(s,
+		jsontext.WithIndent(p.indent),
+		json.WithMarshalers(json.MarshalFuncV1[*dbus.Variant](
+			func(v *dbus.Variant) ([]byte, error) {
+				return json.Marshal(struct {
+					Signature string
+					Value     interface{}
+				}{
+					Signature: v.Signature().String(),
+					Value:     v.Value()})
+			}),
+		))
 	if err != nil {
 		panic(err)
 	}
@@ -63,12 +68,8 @@ func (p *impl) Reply(c *dbus.Call) {
 		ResponseSequence: c.ResponseSequence,
 	}
 	var err error
-	var raw json.RawMessage
-	if p.indent == "" {
-		raw, err = json.Marshal(v)
-	} else {
-		raw, err = json.MarshalIndent(v, "", p.indent)
-	}
+	var raw []byte
+	raw, err = json.Marshal(v, jsontext.WithIndent(p.indent))
 	if err != nil {
 		panic(err)
 	}
